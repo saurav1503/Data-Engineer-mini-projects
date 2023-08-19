3e088b5a76cb22362824fb1b9a9eeddf29b18533
saurav raj <rsaurav411@gmail.com>
2023-08-20 01:57:05 +0530

    Pass Fail Python Project

--- Python Projects/Pass Fail.py
+++ Python Projects/Pass Fail.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-master_csv = pd.read_csv('C:/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master.csv')
-slave_csv = pd.read_csv('C:/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/slave.csv')
+master_csv = pd.read_csv('/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master.csv')
+slave_csv = pd.read_csv('/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/slave.csv')
 print(master_csv)
 master_pass = master_csv[master_csv["RESULTS"].str[-4:] == "Pass"]
 master_fail = master_csv[master_csv["RESULTS"].str[-4:] == "Fail"]
@@ -16,5 +16,5 @@ master_result.rename(columns={'name': 'POD_NAME'}, inplace=True)
 
 print(master_result)
 
-master_fail.to_csv('C:/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master_fail.csv')
-master_result.to_csv('C:/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master_result.csv')
+master_fail.to_csv('/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master_fail.csv')
+master_result.to_csv('/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/master_result.csv')
