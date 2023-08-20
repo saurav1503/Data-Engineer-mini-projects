b0b12ce6bb6f0855562b24c51e0bbede85d4ec1e
saurav raj <rsaurav411@gmail.com>
2023-08-20 11:06:53 +0530

    Birthday Wish Project

---
+++ Python Projects/Monday Motivation.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+# psw:"habawojzognwnwol"
+MY_EMAIL = input("Email:")
+MY_PASSWORD = input("password:")
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("/Users/softy/DataEngineer/Data-Engineer-mini-projects/Data Sets/quote.txt") \
+            as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
+        connection.starttls()
+        connection.login(MY_EMAIL, MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=MY_EMAIL,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
